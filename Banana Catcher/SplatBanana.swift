996e2ce6748017bb98e470bcecb1d29ca7b844f8
Joel Chelliah <joel.chelliah@bekk.no>
2015-10-13 20:12:53 +0200

    Added splat banana animation

--- Banana Catcher/SplatBanana.swift
+++ Banana Catcher/SplatBanana.swift
@@ -4,10 +4,11 @@ import SpriteKit
 class SplatBanana: SKSpriteNode {
 
     init() {
-        let splatDir = Int(arc4random_uniform(2)) + 1
-        let texture = SKTexture(imageNamed: "banana_splat_\(splatDir)")
+        let direction = CGFloat([-1.0, 1.0][Int(arc4random_uniform(2))])
+        let texture = SKTexture(imageNamed: "banana_splat_\(1)")
 
         super.init(texture: texture, color: SKColor.clearColor(), size: texture.size())
+        self.xScale *= direction
         
         self.physicsBody = SKPhysicsBody(texture: self.texture!, size: self.size)
         self.physicsBody?.dynamic = false
@@ -17,6 +18,7 @@ class SplatBanana: SKSpriteNode {
         self.physicsBody?.categoryBitMask = CollisionCategories.Ignorable
         self.physicsBody?.collisionBitMask = CollisionCategories.Ground | CollisionCategories.EdgeBody
         
+        animate()
         decay()
     }
 
@@ -24,10 +26,16 @@ class SplatBanana: SKSpriteNode {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func animate() {
+        let textures = (1...5).map { SKTexture(imageNamed: "banana_splat_\($0).png") }
+        self.runAction(SKAction.animateWithTextures(textures, timePerFrame: 0.05))
+    }
+    
     private func decay() {
+        let wait = SKAction.waitForDuration(1.0)
         let fadeAway = SKAction.fadeAlphaTo(0.0, duration: 1.0)
         let remove = SKAction.runBlock() { self.removeFromParent() }
         
-        self.runAction(SKAction.sequence([fadeAway, remove]))
+        self.runAction(SKAction.sequence([wait, fadeAway, remove]))
     }
 }
