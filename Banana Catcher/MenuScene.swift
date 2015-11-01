282ec73b0272a8cdad063fab51a867eefd2bf4b9
Joel Chelliah <joel.chelliah@bekk.no>
2015-11-01 10:41:08 +0100

    Added menu background and fixed background contrast

--- Banana Catcher/MenuScene.swift
+++ Banana Catcher/MenuScene.swift
@@ -4,12 +4,16 @@ import SpriteKit
 class MenuScene: SKScene {
 
     let newGameNode = "newGame"
+    let ground: Ground = Ground()
+    let basketMan: BasketMan = BasketMan()
     
     override func didMoveToView(view: SKView) {
         backgroundColor = bgColor
+        addBackground()
+        addGround()
+        addBasketMan()
         addTitle()
         addStartBtn()
-        addFallingBananas()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -25,28 +29,50 @@ class MenuScene: SKScene {
         }
     }
     
-    func addTitle() {
+    private func addBackground() {
+        let xPos = CGRectGetMidX(frame)
+        
+        let sky = SKSpriteNode(imageNamed: "menu_sky.png")
+        sky.position = CGPointMake(xPos, sky.size.height / 2)
+        sky.zPosition = -999
+        addChild(sky)
+        
+        let rain = SKEmitterNode(fileNamed: "BananaRain")!
+        rain.position = CGPointMake(xPos, size.height + 50)
+        rain.zPosition = -888
+        addChild(rain)
+        
+        
+        let ground = SKSpriteNode(imageNamed: "menu_ground.png")
+        ground.position = CGPointMake(xPos, ground.size.height / 2)
+        ground.zPosition = -777
+        addChild(ground)
+    }
+    
+    private func addGround() {
+        ground.position = CGPoint(x: CGRectGetMidX(frame), y: ground.size.height / 2)
+        addChild(ground)
+    }
+    
+    private func addBasketMan() {
+        basketMan.position = CGPoint(x: CGRectGetMidX(frame), y: ground.size.height + 10)
+        addChild(basketMan)
+    }
+    
+    private func addTitle() {
         let title = RotatingText(fontNamed: gameFont)
         
         title.setTextFontSizeAndRotate("Banana Catcher", theFontSize: 30)
-        title.position = CGPointMake(size.width/2,size.height/2 + 200)
+        title.position = CGPointMake(size.width / 2, size.height - 100)
         title.fontColor = UIColor.blackColor()
         addChild(title)
     }
     
-    func addStartBtn() {
+    private func addStartBtn() {
         let startGameButton = SKSpriteNode(imageNamed: "newgamebtn")
         
-        startGameButton.position = CGPointMake(size.width / 2, size.height / 2 - 100)
+        startGameButton.position = CGPointMake(size.width / 2, size.height - 200)
         startGameButton.name = newGameNode
         addChild(startGameButton)
-    }
-    
-    func addFallingBananas() {
-        if let rain = SKEmitterNode(fileNamed: "BananaRain") {
-            rain.position = CGPointMake(size.width/2, size.height + 50)
-            rain.zPosition = -1000
-            addChild(rain)
-        }
     }
 }
