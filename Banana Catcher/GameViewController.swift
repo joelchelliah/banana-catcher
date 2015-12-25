1f78c366513921bb5c63ab2d90e9133fe4125f08
Joel Chelliah <joel.chelliah@bekk.no>
2015-12-25 18:12:58 +0100

    Moved global code into seperate file

--- Banana Catcher/GameViewController.swift
+++ Banana Catcher/GameViewController.swift
@@ -1,38 +1,32 @@
 import UIKit
 import SpriteKit
 
-//TODO: move these into a globals file?
-let gameFont: String = "Georgia Bold"
-let musicPlayer = MusicPlayer(music: "menu")
-
-var score: Int = 0
-var soundEnabled: Bool = true
-
-func playSound(node: SKNode, name: String) {
-    if soundEnabled {
-        node.runAction(SKAction.playSoundFileNamed(name, waitForCompletion: false))
-    }
-}
-
-
-
 class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //let scene = MenuScene(size: view.bounds.size)
-        let scene = GameOverScene(size: view.bounds.size)
+        let gameScene = GameOverScene(size: view.bounds.size)
         
-        let skView = view as! SKView
-        skView.showsFPS = false
-        skView.showsNodeCount = false
-        skView.ignoresSiblingOrder = true
+        gameScene.scaleMode = .ResizeFill
         
-        scene.scaleMode = .ResizeFill
-        skView.presentScene(scene)
+        let gameView = initGameView()
+        
+        gameView.presentScene(gameScene)
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    
+    private func initGameView() -> SKView {
+        let gameView = view as! SKView
+        
+        gameView.showsFPS = false
+        gameView.showsNodeCount = false
+        gameView.ignoresSiblingOrder = true
+        
+        return gameView
     }
 }
---
+++ Banana Catcher/Globals.swift
@@ -0,0 +1,15 @@
+import Foundation
+import SpriteKit
+
+
+let gameFont: String = "Georgia Bold"
+let musicPlayer = MusicPlayer(music: "menu")
+
+var score: Int = 0
+var soundEnabled: Bool = true
+
+func playSound(node: SKNode, name: String) {
+    if soundEnabled {
+        node.runAction(SKAction.playSoundFileNamed(name, waitForCompletion: false))
+    }
+}
